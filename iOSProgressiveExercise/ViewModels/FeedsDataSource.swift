04fa3fe7fa8212914d9269f65866a21e878e6bb9
Nischal Hada <hadanischal@gmail.com>
2018-05-29 15:03:27 +1000

    code refactored

--- iOSProgressiveExercise/ViewModels/FeedsDataSource.swift
+++ iOSProgressiveExercise/ViewModels/FeedsDataSource.swift
@@ -12,7 +12,7 @@ import UIKit
 class GenericDataSource<T> : NSObject {
     var data: DynamicValue<[T]> = DynamicValue([])
     fileprivate let landscapeReuseIdentifier = "LandscapeTableViewCell"
-
+    
 }
 
 class FeedsDataSource : GenericDataSource<ListModel>, UITableViewDataSource {
@@ -26,11 +26,18 @@ class FeedsDataSource : GenericDataSource<ListModel>, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: landscapeReuseIdentifier, for: indexPath) as! LandscapeTableViewCell
         let feedsValue = self.data.value[indexPath.row]
-        cell.feedsValue = feedsValue
-        ImageHelper().updateImageForTableViewCell(cell, inTableView: tableView, imageURL:feedsValue.imageRef, atIndexPath: indexPath)
-        return cell
+        if feedsValue.imageRef != "" {
+            let cell = tableView.dequeueReusableCell(withIdentifier: landscapeReuseIdentifier, for: indexPath) as! LandscapeTableViewCell
+            cell.feedsValue = feedsValue
+            ImageHelper().updateImageForTableViewCell(cell, inTableView: tableView, imageURL:feedsValue.imageRef, atIndexPath: indexPath)
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Header")!
+            cell.textLabel?.text = feedsValue.title
+            cell.detailTextLabel?.text = feedsValue.description
+            return cell
+        }
     }
 }
 
